159ad15fd919e01cb4da8a457a4a04436d8efddd
Novita H <novitahamilton@gmail.com>
2023-11-29 14:03:34 -0800

    (1) index.html: - Replace recently-added section with search-result - Update 'type' in <script> to 'module' to export config.js file - Remove <script> for config.js
    (2) Add package.json file to export config.js file (in .gitignore)
    
    (3) script.js
    - Add getToken, searchItem, displaySearchResult, searchOnEnter functions
    - Add element selectors
    - Add eventListener for searchBar

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
 'use strict';
+import { myClientId, myClientSecret } from './config.js';
+
+console.log(myClientId, myClientSecret);
+
+const clientId = myClientId;
+const clientSecret = myClientSecret;
+
+// Function to get token (token expires in 1hr)
+const getToken = async () => {
+  try {
+    const response = await fetch(`https://accounts.spotify.com/api/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    });
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error('Fetch error', error);
+  }
+};
+
+getToken();
+
+// Function for the search bar
+const searchItem = async (token) => {
+  try {
+    const token = await getToken();
+    console.log(token);
+    const response = await fetch(
+      'https://api.spotify.com/v1/search?q=masego&type=artist%2Ctrack%2Calbum',
+      {
+        method: 'GET',
+        headers: { Authorization: 'Bearer ' + token },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Fetch error', error);
+  }
+};
+
+// searchItem();
+
+const displaySearchResult = async () => {
+  const searchResult = await searchItem();
+  console.log(searchResult);
+};
+
+displaySearchResult();
+
+//-----------------------------------Elemement Selector-----------------------------//
+
+const elementSearchBar = document.querySelector('.search-bar');
+const elementSearchResult = document.querySelector('.search-result');
+
+//------------------------------------Event Listener------------------------------//
+
+elementSearchBar.addEventListener('keydown', searchOnEnter); // Capturing 'Enter' on search bar
+
+//-----------Functions---------------//
+
+function searchOnEnter(e) {
+  console.log(e.key);
+  if (e.key === 'Enter') {
+    searchItem();
+  }
+}
